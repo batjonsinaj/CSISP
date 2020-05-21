34af04711782867d6e0e5702b880ef41db0c844b
batjonsinaj <43096541+batjonsinaj@users.noreply.github.com>
2020-05-21 10:36:38 -0400

    app.vue v1

---
+++ src/App.vue
@@ -0,0 +1,147 @@
+<!-- App.vue buefy navbar of all tabs-->
+<!-- Preloading colour type settings-->
+<template>
+    <div>
+        <b-navbar class="is-second navbar">
+            <template slot="brand">
+            </template>
+            <template slot="start">
+                <b-navbar-item tag="router-link" :to="{ path: '/' }">Welcome</b-navbar-item>
+                <b-navbar-item tag="router-link" :to="{ path: '/homePage' }">Home</b-navbar-item>
+                <b-navbar-dropdown label="Actions" >
+                    <b-navbar-item tag="router-link" :to="{ path: '/Leaderboard' }" class="is-first navbar">Leaderboard</b-navbar-item>
+                    <b-navbar-item tag="router-link" :to="{ path: '/aboutUS' }" class="is-first navbar">About us</b-navbar-item>
+                </b-navbar-dropdown>
+            </template>
+            <template slot="end">
+                <b-navbar-item tag="div">
+                    <div class="buttons">
+                        <a class="is-first button" @click.prevent="reset()">
+                        <strong>Reset Account</strong>
+                        </a>
+                    </div>
+                </b-navbar-item>
+            </template>
+        </b-navbar>
+        <br />
+        <transition name="fade">
+            <router-view></router-view>
+        </transition>
+    </div>
+</template>
+
+<style lang="scss">
+// Import Bulma's core
+@import "~bulma/sass/utilities/_all";
+
+// Set your colors
+$primary: #8c67ef;
+$primary-invert: findColorInvert($primary);
+$twitter: #4099ff;
+$twitter-invert: findColorInvert($twitter);
+
+$first: #9a841c;
+$first-invert: findColorInvert($first);
+$second: #482e06;
+$second-invert: findColorInvert($second);
+$third: #f7e79c;
+$third-invert: findColorInvert($third);
+// Setup $colors to use as bulma classes (e.g. 'is-twitter')
+$colors: (
+  "white": ($white, $black),
+  "black": ($black, $white),
+  "light": ($light, $light-invert),
+  "dark": ($dark, $dark-invert),
+  "primary": ($primary, $primary-invert),
+  "info": ($info, $info-invert),
+  "success": ($success, $success-invert),
+  "warning": ($warning, $warning-invert),
+  "danger": ($danger, $danger-invert),
+  "twitter": ($twitter, $twitter-invert),
+  "first": ($first, $first-invert),
+  "second": ($second, $second-invert),
+  "third": ($third, $third-invert)
+);
+
+// Links
+$link: $primary;
+$link-invert: $primary-invert;
+$link-focus-border: $primary;
+
+// Navbar height
+$navbar-item-img-max-height: 3rem !important;
+
+// Import Bulma and Buefy styles
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
+
+// Classes
+.container {
+  padding: 50px;
+  background-color: #9a841c;
+  border-radius: 25px;
+}
+
+.home-header {
+  color: white;
+  font-size: 40px;
+  font-weight: 1000;
+}
+
+.home-second {
+  color: white;
+  font-size: 30px;
+  font-weight: 1000;
+}
+
+body {
+  background-color: #f7e79c;
+  background-size: 100%;
+  height: 1200px;
+}
+
+</style>
+
+<script>
+export default {
+    data() {
+        return {
+            transactions: []
+        }
+    }, created(){
+        let uri = 'http://localhost:4000/transactions';
+        this.axios.get(uri).then(response => {
+            this.transactions = response.data;
+        });
+
+        uri = 'http://localhost:4000/currencies/initializeAll';
+        this.axios.post(uri).then(() => {
+            console.log("Currencies initialized!");
+        });
+
+    },
+    methods: {
+        reset(){//grab the id's of all transactions and delete them from DB, this leaves no items so no balanceAfter and no balance in bank
+            for(var t of this.transactions){
+                let uri = `http://localhost:4000/transactions/delete/`+t._id;
+                this.axios.delete(uri).then(response => {
+                this.transactions = response.data;
+                });
+            }
+            this.$buefy.snackbar.open({
+                message: `Press reset once more to complete the action.`,
+                position: 'is-top'
+            })
+            console.log("successfully reset account");
+             location.reload();
+
+             // for(var c of this.currencies){
+             //    let uri = `http://localhost:4000/currencies/delete/`+c._id;
+             //    this.axios.delete(uri).then(response => {
+             //    this.currencies = response.data;
+             //    });
+            // }
+        }
+    }
+};
+</script>
